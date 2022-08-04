3b3dd38df70a3867fe22fc4744d7ce62d52c2900
FlaserHost <31478664+FlaserHost@users.noreply.github.com>
2022-08-04 20:08:53 +0300

    Создан файл миграции

---
+++ migrations/m220804_170541_add_urltable.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220804_170541_add_urltable
+ */
+class m220804_170541_add_urltable extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220804_170541_add_urltable cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220804_170541_add_urltable cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
