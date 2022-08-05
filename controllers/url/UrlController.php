2d932594ab158afafc52c0c514ea32d91db1720d
FlaserHost <31478664+FlaserHost@users.noreply.github.com>
2022-08-05 10:06:39 +0300

    httpclient

--- controllers/url/UrlController.php
+++ controllers/url/UrlController.php
@@ -4,6 +4,7 @@ namespace app\controllers\url;
 
 use app\models\url\UrlForm;
 use yii\web\Controller;
+use yii\httpclient\Client;
 
 class UrlController extends Controller
 {
@@ -14,5 +15,36 @@ class UrlController extends Controller
         $this->view->title = 'Сервис проверки доступности URL-ов';
         $this->view->registerMetaTag(['name' => 'description', 'content' => 'Сервис проверки доступности url-ов'], 'description');
         return $this->render('/url/mainView', compact('urlForm'));
+    }
+
+    public function actionCheck()
+    {
+        $request = \Yii::$app->request;
+        if($request->post("url") && $request->post("frequency") && $request->post("repeat_count"))
+        {
+            if($request->isAjax)
+            {
+                $url = $request->post("url");
+
+
+
+
+
+                /*$client = new Client([
+                    'transport' => 'yii\httpclient\CurlTransport'
+                ]);
+
+                $response = $client->createRequest()
+                    ->setMethod('POST')
+                    ->setUrl('http://domain.com/api/1.0/users')
+                    ->setData(['name' => 'John Doe', 'email' => 'johndoe@domain.com'])
+                    ->setOptions([
+                        CURLOPT_CONNECTTIMEOUT => 10, // тайм-аут подключения
+                        CURLOPT_TIMEOUT => 10, // тайм-аут получения данных
+                    ])
+                    ->send();*/
+
+            }
+        }
     }
 }
--- views/url/mainView.php
+++ views/url/mainView.php
@@ -9,21 +9,13 @@
             'id' => 'urlForm',
     ]) ?>
     <div class="fieldSet">
-        <?= $inputForm->field($urlForm, 'url')->textInput(['placeholder' => 'Введите URL для проверки']) ?>
-        <?= $inputForm->field($urlForm, 'frequency')->input('number', ['placeholder' => 'Введите частоту проверки']) ?>
-        <?= $inputForm->field($urlForm, 'repeat_count')->input('number', ['placeholder' => 'Введите количество повторов при ошибке']) ?>
+        <?= $inputForm->field($urlForm, 'url')->textInput(['name' => 'url', 'placeholder' => 'Введите URL для проверки']) ?>
+        <?= $inputForm->field($urlForm, 'frequency')->input('number', ['name' => 'frequency', 'placeholder' => 'Введите частоту проверки']) ?>
+        <?= $inputForm->field($urlForm, 'repeat_count')->input('number', ['name' => 'repeat_count', 'placeholder' => 'Введите количество повторов при ошибке']) ?>
         <?= Html::submitButton('Проверить', ['class' => 'btn btn-primary btn-block', 'id' => 'submitBtn']) ?>
     </div>
     <?php ActiveForm::end() ?>
 </div>
-
-
-
-
-
-
-
-
 <?php
     // Отключение стандартного тулбара от Yii2
     if(class_exists('yii\debug\Module'))
